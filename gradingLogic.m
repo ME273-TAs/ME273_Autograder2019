405a6e64c32a15076f85419e9e9f951341d0316b
jaredhmt <jaredhmt@gmail.com>
2020-08-29 21:34:23 -0600

    Change Grading Logic Structure. - Simplified gradingLogic tree. - ensured some branches of the tree work. - more vigourous testing still to follow

--- gradingLogic.m
+++ gradingLogic.m
@@ -1,6 +1,6 @@
 function [feedbackFlag, gradingAction] = gradingLogic(File, ...
     CurrentDeadline, OldLate, OldFeedbackFlag, OldScore, pseudoDate, ...
-    regrading, configVars, FirstDeadline)
+    finalGrading, configVars, FirstDeadline)
 %============================================BEGIN-HEADER=====
 % FILE: gradingLogic.m
 % AUTHOR: Caleb Groves
@@ -18,7 +18,7 @@ function [feedbackFlag, gradingAction] = gradingLogic(File, ...
 %   OldFeedbackFlag - previous feedback flag
 %   OldScore - previous score recorded for current lab part
 %   pseudoDate - "now" for the program
-%   regrading - regrade flag
+%   finalGrading - regrade flag
 %   configVars - configuration variables from configAutograder.m
 %   FirstDeadline - the original deadline for this student -- used for
 %   regrading mode
@@ -26,11 +26,10 @@ function [feedbackFlag, gradingAction] = gradingLogic(File, ...
 %
 % OUTPUTS:
 %   gradingAction - integer corresponding to specific grading actions
-%   1 = normal grading
+%   1 = feedback grading
 %   2 = copy over the old scores and feedback
-%   3 = late grading
+%   3 = final grading
 %   4 = no-submit, 1st warning
-%   6 = late submission Feedback Grading - Added by Jared H. 12/28/19
 %
 %   feedbackFlag - new feedback flag to record
 %
@@ -42,11 +41,6 @@ function [feedbackFlag, gradingAction] = gradingLogic(File, ...
 % VERSION HISTORY TRACKED WITH GIT
 %
 %==============================================END-HEADER======
-if pseudoDate < CurrentDeadline  % 'now' is before this student's deadline
-    feedbackFlag = OldFeedbackFlag;
-    gradingAction = 2;
-    return;
-end
 
 % if isstruct(File)
 %     if datetime(File.date) > FirstDeadline
@@ -57,52 +51,39 @@ end
 %     end
 % end
 
+numSubmissions = 1;
+chances = 2;
+lastSubDate = pseudoDate - 30;
+
 if isstruct(File) % filter students that haven't submitted
     if datetime(File.date) <= CurrentDeadline % make sure file was submitted before deadline
-        if regrading
-            % changed the following if statement to reflect new regrading policy
-            % Jared Oliphant 1/22/2019
-            % see also 'dynamicToStatic.m' and 'get_lab_part_score.m'
-            % and 'configAutograder.m'
-            if OldScore < configVars.weights.latePenalty && datetime(File.date) > FirstDeadline 
-                % 12/28/19 Jared H ommit && ~OldLate to account for changes
-                % made to code about late submissions still getting
-                % feedback before final due date
-                
-                % if it satisfies the regrading criteria    (after the
-                % firstDeadline means it is a resubmission.
-                gradingAction = 3; % late grading
-                feedbackFlag = 1;
-            else % if it doesn't satisfy the regrading criteria
-                gradingAction = 2;  % copy over the old scores and feedback
-                feedbackFlag = OldFeedbackFlag;
-            end
-        else % if the program is not running in regrading mode
+        if finalGrading
+            % if the grader is in finalGrading mode, set grading action
+            gradingAction = 3;
+            feedbackFlag = OldFeedbackFlag;
+        else % if the program is not running in finalGrading mode
             if OldScore == 0 % if there is no score
                 gradingAction = 1; % perform original grading
                 feedbackFlag = 1;
+            elseif numSubmissions < chances % if the student has not exceeded feedback limitations
+                if File.date > lastSubDate
+                    gradingAction = 1;
+                    feedbackFlag = 1;
+                else % if the student has exceeded feedback limitations
+                    gradingAction = 2;
+                    feedbackFlag = OldFeedbackFlag;
+                end
             else % if there is a pre-existing score
                 gradingAction = 2; % copy over the old scores and feedback
                 feedbackFlag = OldFeedbackFlag;
             end
         end
-        
-    elseif OldScore == 0 && (datetime(File.date) <= FirstDeadline + 7)
-        % Added by Jared Hale on Dec. 28, 2019.
-        % This elseif will grade late submissions as they come in so
-        % student's code can still be graded for feedback and a grade
-        % before waiting a week for the resubmission deadline.
-        gradingAction = 3;
-        feedbackFlag = 1;
-    else % if the file was submitted after the deadline
-        gradingAction = 2; % copy over the old scores and feedback
-        feedbackFlag = OldFeedbackFlag;
     end
 else % if no file was submitted
-    if OldFeedbackFlag == 0
+    if OldScore == 0
         gradingAction = 4; % no-submit, 1st warning
         feedbackFlag = 1;
-    elseif OldFeedbackFlag ~= 0
+    elseif OldScore ~= 0
         gradingAction = 2; % copy over the old scores and feedback
         feedbackFlag = OldFeedbackFlag;
     end
